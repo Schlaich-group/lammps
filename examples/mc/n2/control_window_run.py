6924d6285f516ea818821aadd0fe3cdabf997ade
Philipp Stärk <philipp.staerk@t-online.de>
2023-01-27 17:03:11 +0100

    Window setup script.

---
+++ examples/mc/n2/control_window_run.py
@@ -0,0 +1,128 @@
+import jinja2
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as constants
+import os
+import shutil
+import multiprocessing as mp
+
+WINDOW_MIN = 0
+WINDOW_MAX = 180
+WINDOW_LENGTH = 30
+NR_OVERLAP = 5
+INITIAL_F = np.exp(4)**(1/2**6)
+F_STEP_MAX = 15
+ACCURACY_FACTOR = 500
+
+NR_STEPS_MC = 100000
+NR_THREADS = 4
+
+TEMPLATE_FILENAME = 'in.wang_landau.n2.template'
+
+# pick the middle of the window
+START_NR = (WINDOW_MAX - WINDOW_MIN) // 2
+
+def run_everything(window):
+    # create a dir for the window
+    if not os.path.exists(f'window_{window[0]}_{window[1]}'):
+        os.mkdir(f'window_{window[0]}_{window[1]}')
+
+    # change to the dir
+    os.chdir(f'window_{window[0]}_{window[1]}')
+
+    # also copy the template file
+    shutil.copy(f'../{TEMPLATE_FILENAME}', '.')
+
+    windows = np.arange(window[0], window[1]+1)
+
+    # write the windows to a file, together with a number of ones (qs) and zeros (hs)
+    # delimited by a tab
+    np.savetxt('qs.dat', np.column_stack((windows, np.zeros_like(windows),
+                                          np.zeros_like(windows))),
+               delimiter='\t')
+
+    # create the template for the lammps input file from in.wang_landau.template
+    template = jinja2.Template(open(TEMPLATE_FILENAME).read())
+
+
+    # render the initial f and number of steps
+    def create_and_run(f_fac, steps, min_n=window[0], max_n=window[1],
+                       read_data=False, start_n=0):
+        with open('in.wang_landau', 'w') as f:
+            f.write(template.render(f_fac=f_fac, steps=steps, min_n=min_n,
+                                    max_n=max_n, read_data=read_data,
+                                    start_n=start_n))
+
+        # run lammps
+        import subprocess
+        # subprocess call without output
+        subprocess.call(['../../../../build/lmp', '-in', 'in.wang_landau'], 
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # subprocess.call(['../../../../build/lmp', '-in', 'in.wang_landau'])
+
+
+    def convergence_check(f):
+        # Do a convergence check on qs.dat, see if the histogram is flat (h.min == 1/ln(f))
+        hist = np.loadtxt('qs.dat', delimiter='\t')
+        return hist[:, 2].min() > ACCURACY_FACTOR / np.sqrt(np.log(f))
+
+
+    def reset_hs():
+        qs = np.loadtxt('qs.dat', delimiter='\t')
+        qs[:, 2] = 0
+        np.savetxt('qs.dat', qs, delimiter='\t')
+
+    create_and_run(INITIAL_F, 100000, start_n=10)
+    # print(convergence_check(INITIAL_F))
+
+    f = INITIAL_F
+    f_update = 1
+    print(f'Starting to run, f is {f} we are at f_{f_update}')
+    while True:
+        # check for convergence, if converged, make f more fine grained by sqrt(f)
+        if convergence_check(f):
+            # write out the qs to its own file, per f_update
+            np.savetxt(f'qs_f_{f_update}.dat', np.loadtxt('qs.dat',
+                                                          delimiter='\t'))
+
+            f = np.sqrt(f)
+            f_update += 1
+
+            # if f is too fine grained, break
+            if f_update > 15:
+                break
+
+            reset_hs()
+            print(f'New update to f, f is now {f} we are at f_{f_update}')
+            print(f'This means we will step through {NR_STEPS_MC} steps')
+
+        print(f'Running with f={f} for {NR_STEPS_MC} steps')
+        create_and_run(f, NR_STEPS_MC, read_data=True)
+
+    def omega(f, temp, Qarr):
+        """ all energies in Kelvin
+            fugacity in bar!
+        """
+        mass = 28 * constants.atomic_mass
+        db = constants.Planck / np.sqrt(2*np.pi * mass * constants.Boltzmann * temp)
+        mu = temp * np.log((f*constants.bar)*db**3/(constants.Boltzmann*temp))
+        N = Qarr[:, 0]
+        lnQ = Qarr[:, 1]
+        return (-temp*lnQ - mu*N)
+
+    # change back to the main dir
+    os.chdir('..')
+
+
+# partition the min max into WINDOWS_LENGTH steps, with NR_OVERLAP overlap
+step = WINDOW_LENGTH - NR_OVERLAP
+left, right = 0, WINDOW_LENGTH
+window = []
+while right < WINDOW_MAX:
+    left += step
+    right += step if right + step < WINDOW_MAX else WINDOW_MAX
+    window.append((left, right))
+
+# Run through the simulations in parallel
+with mp.Pool(NR_THREADS) as p:
+    p.map(run_everything, window)
